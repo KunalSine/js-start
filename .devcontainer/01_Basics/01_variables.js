16f004df802c25b05cab235913c48d235e915632
KunalSine <118209839+KunalSine@users.noreply.github.com>
2025-01-18 09:30:04 +0000

    continue

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountPassword = "12222"
+let accountEmail = "neo@google" 
+accountCity = "Punjab"
+let accountState
+
+    // account = 2 (not allowed because its constant, never changes)
+console.log(accountId)
+
+accountPassword  = "10000"
+accountEmail = "hc@hc.com"
+accountCity = "Himachal"
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
