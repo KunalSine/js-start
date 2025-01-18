0ab58bcb580ab6e2870c8e80374596429fbbfccf
KunalSine <118209839+KunalSine@users.noreply.github.com>
2025-01-18 17:39:38 +0000

    continue

--- .devcontainer/01_Basics/02_datatypes.js
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -1,3 +1,23 @@
 "use strict"; // treat all JS code as newer version//
 
-alert(3+3)
+// alert(3+3) // we are using node js
+
+let name = "Kunal" // string
+let age = 18 // number
+let isLoggedIn = false // boolean
+
+// number => 2 to power 53
+// bigint (big numbers)
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined => (let State;)
+// symbol => unique
+
+// object
+
+console.log(typeof "kunal");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
