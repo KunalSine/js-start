4dd679738f7ebcff8f8e852d091117da8f3b98c2
KunalSine <118209839+KunalSine@users.noreply.github.com>
2025-01-18 18:06:20 +0000

    CONTINUE

--- .devcontainer/01_Basics/02_datatypes.js
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -1,11 +1,13 @@
 "use strict"; // treat all JS code as newer version//
 
 // alert(3+3) // we are using node js
+/*
 
 let name = "Kunal" // string
 let age = 18 // number
 let isLoggedIn = false // boolean
 
+/*
 // number => 2 to power 53
 // bigint (big numbers)
 // string => ""
@@ -16,8 +18,40 @@ let isLoggedIn = false // boolean
 
 // object
 
-console.log(typeof "kunal");
+/*console.log(typeof "kunal");
 console.log(typeof age);
 console.log(typeof isLoggedIn);
 console.log(typeof null);
-console.log(typeof undefined);
+console.log(typeof undefined);*/
+
+    // CONVERSION OPERATIONS
+// 1. NUMBERS
+
+//let score = null
+let score = true
+//let score = false
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // NaN= not a no.
+
+// 2. BOOLEAN
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true , 0=> flase
+// "" => false
+// "kunal" => true
+
+// 3. STRING 
+
+let someNumber = 30
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
